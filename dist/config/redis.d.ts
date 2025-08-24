784af96281eb00a3253600d7d110f363872a9f2a
X-culture24 <arthurarcade42@gmail.com>
2025-08-24 15:40:33 +0300

    Remove node_modules from tracking

--- dist/config/redis.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const connectRedis: () => Promise<void>;
-export declare const getRedisClient: () => any;
-export declare const disconnectRedis: () => Promise<void>;
-export default getRedisClient;
-//# sourceMappingURL=redis.d.ts.map
